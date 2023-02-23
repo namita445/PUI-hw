2dfd221f8aa1ab6dc98709a692893bcea9e9774a
Namita <41166136+namita445@users.noreply.github.com>
2023-02-23 12:53:58 -0500

    fix

--- hw4/js/price.js
+++ hw4/js/price.js
@@ -1,3 +1,48 @@
+
+/* Rolls database */
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+/* URL params */
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const chosenRoll = params.get('roll');
+
+const imgname = rolls[chosenRoll].imageFile;
+const productImage = document.querySelector('#detail-img');
+productImage.src = 'products/' + imgname;
+
+const productTitle = document.querySelector('#detail-title');
+productTitle.innerText = chosenRoll + " Cinnamon Roll";
+
+
+/* Price computation */
+
 let glazingOptions = {
     "Keep original": 0.00,
     "Sugar milk": 0.00,
@@ -15,9 +60,8 @@ let packsizeOptions = {
 //set defaults
 let currentGlaze = glazingOptions["Keep original"];
 let currentPack = packsizeOptions[1];
-//whyyyy
-let base = 2.49;
 
+let base = rolls[chosenRoll].basePrice;
 
 let selectGlaze = document.querySelector("#glazingOptions");
 let selectPack = document.querySelector("#packsizeOptions");
